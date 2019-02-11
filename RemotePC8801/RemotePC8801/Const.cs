d52083b0e1e076735ce49e97bcb846ebb15c10c6
Akira Kawamata <autumn@piedey.co.jp>
2019-02-11 16:48:57 +0900

    fix sector offset

--- RemotePC8801/RemotePC8801/Const.cs
+++ RemotePC8801/RemotePC8801/Const.cs
@@ -10,7 +10,7 @@ namespace RemotePC8801
     {
         public static string ClearErrStatement { get { return "poke &he649,0"; } }
         public static string GettingVersionExpression { get { return "chr$(peek(&h79d5))+chr$(peek(&h79d6))+chr$(peek(&h79d7))"; } }
-        public static string SectorReadStatements { get { return ":PRINT \"%%%\";:a=VARPTR(#0):" + CommonReadStatements; } }
+        public static string SectorReadStatements { get { return ":PRINT \"%%%\";:a=VARPTR(#0)+9:" + CommonReadStatements; } }
         public static string RomReadStatements { get { return ":PRINT \"%%%\";:a=0:" + CommonReadStatements; } }
         //public static string CommonReadStatements { get { return "FOR I=0 to 255:V=PEEK(a+i):T$=chr$(V)+\" \"+CHR$((V+&H20) and &hFF):N=(V<=&h20)*-1:L=N+1:PRINT MID$(T$,N+1,L);:NEXT:PRINT"; } }
 
