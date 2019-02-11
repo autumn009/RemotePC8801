76b8a10156c01da00a4d8b650dd483f6d075aecf
Akira Kawamata <autumn@piedey.co.jp>
2019-02-11 15:01:25 +0900

    auto save kicker

--- RemotePC8801/RemotePC8801/PageStart.xaml.cs
+++ RemotePC8801/RemotePC8801/PageStart.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,30 @@ namespace RemotePC8801
         public PageStart()
         {
             InitializeComponent();
+        }
+
+        private async void AutoSave_Click(object sender, RoutedEventArgs e)
+        {
+            const string data = @"10 rem auto start program for Remove PC-8801 client
+20 console 0,25,1,1
+30 cls 1
+40 print ""TERM MODE""
+50 color 2:print ""SHIFT+STOP"";:color 7:print ""to quit TERM mode.""
+60 term ""N81XNF"",,8192
+";
+
+            using (var diable = new LockForm())
+            {
+                if (await Util.SendCommandAsyncAndErrorHandle("open \"" + Util.GetSelectedDrive() + ":rem88\" for output as #1 ")) return;
+                var reader = new StringReader(data);
+                for (; ; )
+                {
+                    var s = reader.ReadLine();
+                    if (s == null) break;
+                    if (await Util.SendCommandAsyncAndErrorHandle("print #1,\"" + s.Replace("\"", "\"+chr$(34)+\"") + "\"")) return;
+                }
+                if (await Util.SendCommandAsyncAndErrorHandle("close #1")) return;
+            }
         }
     }
 }
